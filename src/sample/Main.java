33b9e724a13ed29750c8af0732f2e87e196e751d
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2019-05-15 15:10:59 +0400

    create structure project

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Регистрация заявок");
+        primaryStage.setScene(new Scene(root, 1000, 575));
         primaryStage.show();
     }
 
