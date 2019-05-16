8ddb22b2bbfada824606fa834a745b9e78e08e3a
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2019-05-16 08:38:57 +0400

    started Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,24 @@
 package sample;
 
+import javafx.scene.control.*;
+
 public class Controller {
-  //  public void sendData(){}
+    public DatePicker dataPicker;
+    public TextField timeTextField;
+    public ComboBox implementerTextField;
+    public ComboBox senderTextField;
+    public Label labelDate;
+    public Label labelTime;
+    public Label labelSender;
+    public Label labelImplementer;
+    public Label labelStatus;
+
+    public TextArea descriptionTextArea;
+    public ComboBox statusTextFiled;
+    public Label labelDescription;
+
+
+    public void sendData(){
+
+    }
 }
